98dc62bc73667096e12851b9bf26cab18bd59cdf
Nicolás <nick.mdz.b@gmail.com>
2023-09-20 11:09:04 -0600

    Actualizando al Proyecto Finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
 
-  const [pacientes,setPacientes] = useState([]);
+  const [pacientes,setPacientes] = useState(JSON.parse(localStorage.getItem("pacientes")) ?? []);
   const [paciente,setPaciente] = useState({});
+  //useEffect para Local Storage
+  // useEffect( () => {
+  //   const obtenerLS = () => {
+  //     const pacientesLS = JSON.parse(localStorage.getItem("pacientes")) ?? [];
+  //     setPacientes(pacientesLS)
+  //   }
+  //   obtenerLS();
+  // }, []);
+
+  useEffect( () => {
+    localStorage.setItem("pacientes", JSON.stringify(pacientes));
+  },[pacientes])
+
+  //Función que elimina pacientes
+  const eliminarPaciente = id => {
+    const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id);
+    setPacientes(pacientesActualizados)
+  }  
 
   return (
     <div className="container mx-auto mt-10">
@@ -16,10 +34,12 @@ function App() {
         pacientes={pacientes}
         setPacientes={setPacientes}
         paciente={paciente}
+        setPaciente={setPaciente}
       />
       <ListadoPacientes 
         pacientes={pacientes}
         setPaciente={setPaciente}
+        eliminarPaciente={eliminarPaciente}
       />
     </div>
 
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import Error from "./Error";
 
-const Formulario = ( {pacientes, setPacientes, paciente} ) => {
+const Formulario = ( {pacientes, setPacientes, paciente, setPaciente} ) => {
   //useStates
   const [nombre,setNombre] = useState("");
   const [propietario,setPropietario] = useState("");
@@ -10,9 +10,15 @@ const Formulario = ( {pacientes, setPacientes, paciente} ) => {
   const [sintomas,setSintomas] = useState("");
   //State para error
   const [error,setError] = useState(false)
-
+  //useEffect
   useEffect(() => {
-    console.log(paciente)
+    if ( Object.keys(paciente).length>0 ) { //Una forma de saber si un objeto está vacío o no
+      setNombre(paciente.nombre)
+      setPropietario(paciente.propietario)
+      setEmail(paciente.email)
+      setFecha(paciente.fecha)
+      setSintomas(paciente.sintomas)
+    }
   }, [paciente])
   //Función para el id único de cada objeto de Paciente
   const generarId = () => {
@@ -37,11 +43,24 @@ const Formulario = ( {pacientes, setPacientes, paciente} ) => {
         propietario,
         email,
         fecha,
-        sintomas,
-        id: generarId()
+        sintomas
       }
 
-      setPacientes([...pacientes, objetoPaciente]);
+      if (paciente.id) {
+        //Editando el registro
+        objetoPaciente.id = paciente.id
+        const pacientesActualizados = pacientes.map( pacienteState => pacienteState.id === 
+        paciente.id ? objetoPaciente : pacienteState )
+
+          setPacientes(pacientesActualizados)
+          setPaciente({})//Para limpiar el state en memoria
+      }else{
+        //Nuevo registro
+        objetoPaciente.id = generarId();
+        setPacientes([...pacientes, objetoPaciente]);
+      }
+
+      
       //Reiniciar el form
       setNombre("")
       setPropietario("")
@@ -149,7 +168,7 @@ const Formulario = ( {pacientes, setPacientes, paciente} ) => {
             type="submit"
             className='bg-indigo-600 w-full p-3 text-white uppercase font-bold 
             hover:bg-indigo-700 cursor-pointer transition-colors'
-            value="Agregar Paciente"
+            value={paciente.id ? "Editar Paciente" : "Agregar Paciente"} //Si paciente contiene id, entonces editar paciente, si no, agregar paciente (forma simple de condicionar el texto dependiendo de si estamos editando o no al paceinte)
         />
       </form>
     </div>
